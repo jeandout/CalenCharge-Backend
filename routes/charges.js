a5899e6eb1d45a91e8914f68c88867f608de6fa2
jeandout <jeandout@hotmail.fr>
2024-12-13 10:55:38 +0100

    starting model route

--- routes/charges.js
+++ routes/charges.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const User = require('../models/users');
+
+
+//CRUD on charges
+
+router.get('/', (req, res) => {
+  User.findOne({ email: req.body.email }).then(data => {
+
+    if (data) {
+     
+      // let format = []
+      // for (let el of data.places) {
+      //   el.nickname = req.params.nickname
+      //   format.push(el)
+      // }
+      // console.log(format)
+      res.json({ result: true, obj: data.accounts.obj });
+    } else {
+      res.json({ result: false, error: 'User not found' });
+    }
+  })
 });
 
 module.exports = router;
-
-//CRUD on charges
