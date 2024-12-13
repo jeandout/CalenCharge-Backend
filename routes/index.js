35854e8d97ddfd867aff0f088e0951f0626f2e24
Lucie Masterson <lucie.masterson@gmail.com>
2024-12-13 16:22:16 +0100

    update signin to return account data and delete index route

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,9 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
-const User = require('../models/users');
-const Account = require('../models/accounts');
-require('dotenv').config();
+var express = require('express');
+var router = express.Router();
 
-const router = express.Router();
-const SECRET_KEY = process.env.JWT_SECRET_KEY;
-
-// GET infos of authenticated user
-router.get('/user', passport.authenticate('jwt', { session: false }), async (req, res) => {
-
-  try {
-    const user = await User.findById(req.user._id).populate('accounts');
-
-    if (!user) {
-      return res.status(404).json({ message: 'Utilisateur non trouvé' });
-    }
-    res.status(200).json({ result:true, user});
-  } catch (err) {
-    res.status(500).json({result:false, message: 'Erreur lors de la synchronisation des données', error: err.message });
-  }
-
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
